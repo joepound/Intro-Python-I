de6786d4afd1dd1b58d526eddba619d72b436ba7
joepound <14357797+joepound@users.noreply.github.com>
2019-09-03 18:36:33 -0600

    Completed exercise 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -1,6 +1,6 @@
 """
 Python makes performing file I/O simple. Take a look
-at how to read and write to files here: 
+at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
@@ -9,10 +9,28 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open("src/foo.txt") as fooFile:
+    for line in fooFile:
+        print(line, end="")
+    fooFile.close()
+
+print("\n")  # Line break to visually separate next output
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open("src/bar.txt", "w") as barFile:
+    barFile.write("Line One\n")
+    barFile.write("Line Two\n")
+    barFile.write("Line Three\n")
+    barFile.close()
+
+with open("src/bar.txt") as barFile:
+    for line in barFile:
+        print(line, end="")
+    barFile.close()
